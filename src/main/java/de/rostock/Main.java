5059fe33d76c467294e921bf86d6e06b6b075a66
SaboteurInCave <saboteurincave@gmail.com>
2016-06-08 19:27:29 +0300

    Initial commit

---
+++ src/main/java/de/rostock/Main.java
@@ -0,0 +1,8 @@
+package de.rostock;
+
+public class Main {
+    public static void main(String[] args) {
+        Model model = new Model(args[0]);
+        model.parseModel();
+    }
+}
---
+++ src/main/java/de/rostock/Model.java
@@ -0,0 +1,16 @@
+package de.rostock;
+
+public class Model {
+    private String path;
+
+    public Model(String path) {
+        this.path = path;
+    }
+
+
+    public void parseModel() {
+        /*
+            TODO: Write parse xml method and write model to output
+         */
+    }
+}
